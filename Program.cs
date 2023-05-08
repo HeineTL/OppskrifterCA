081823e1301b4c5f5a1dbc49b77e9eb982bf6806
HeineTL <116063325+HeineTL@users.noreply.github.com>
2023-05-08 11:03:26 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OppskrifterCA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
